a3b09d541238058ac91d3c20147b44b3f695b49a
bija510 <error.zxz@gmail.com>
2021-03-26 23:03:33 -0400

    test after projectName change

--- cypress/integration/TestCases/zTemp.js
+++ cypress/integration/TestCases/zTemp.js
@@ -1,14 +1,14 @@
 /// <reference types="Cypress"/>
 
-describe('zTemp', function() {
+describe('zTemp', () => {
 
-    it('testSpec', function() {
-        cy.visit("http://demo.automationtesting.in/Register.html")
+    it('testSpec', () => {
+        cy.visit("http://admin:admin@the-internet.herokuapp.com/")
         cy.get(":nth-child(1) > :nth-child(2) > .form-control").type("David").should('have.value', 'David')
             // cy.get(':nth-child(1) > .ng-pristine').type()
         cy.scrollTo('bottom')
         cy.wait(3000)
         cy.scrollTo('top')
-    })
 
+    })
 })
